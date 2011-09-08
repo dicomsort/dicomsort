0d487f7cecc3984bde2bdf76ce64a844c39a9b7e
Suever <suever@gmail.com>
2011-09-08 16:18:12 -0400

    Updated setup.py for compilation on windows platforms as well as innofile for generating installation wizard on Windows platforms

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import py2exe
+import os
+import gui
+import shutil
+
+data = [('',['build/DSicon.ico',]),
+		('',['build/msvcp90.dll',])]
+
+NAME = 'DICOM Sort'
+VER	= gui.__version__
+
+OUTDIR = os.path.expanduser(os.path.join('~','Desktop',''.join([NAME,' ',VER])))
+		
+setup(
+	name = NAME,
+	version = VER,
+	description = 'A DICOM Sorting Utility',
+	author = 'Jonathan Suever',
+	packages=['gui',],
+    options = {'py2exe': {'bundle_files': 1,
+						  'dist_dir':OUTDIR}},
+	data_files = data,
+    windows = [{"script":'DicomSort.pyw',
+                "dest_base":NAME,
+				"icon_resources":[(1,"DSicon.ico")]}],
+    zipfile = None,
+)
+
+shutil.rmtree(os.path.join(OUTDIR,'tcl'))
+os.remove(os.path.join(OUTDIR,'w9xpopen.exe'))
