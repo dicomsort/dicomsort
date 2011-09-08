f20059febcb647f324348280022c4fd75eb1dda5
Suever <suever@gmail.com>
2011-09-08 16:24:43 -0400

    Updated setup.py file for compilation on Windows

--- build/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(
-    options = {'py2exe': {'bundle_files': 1}},
-    windows = [{"script":'DicomSort.pyw',"icon_resources":[(1,"DSicon.ico")]}],
-    zipfile = None,
-)
