3c839169704fc191a7339e9fb435853841c25ede
Jonathan Suever <suever@gmail.com>
2016-06-15 11:00:45 -0400

    Increments version number

--- gui/__init__.py
+++ gui/__init__.py
@@ -21,7 +21,7 @@ if os.name == 'nt':
 else:
     configFile = os.path.join(os.getenv("HOME"), '.dicomSort.ini')
 
-Version = (2, 1, 7)
+Version = (2, 1, 8)
 __version__ = '.'.join([str(x) for x in Version])
 
 configVersion = '2.0'
