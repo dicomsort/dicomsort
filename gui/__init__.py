18ec80bb9d8793f0385b67b23c265e7895f3bd14
Jonathan Suever <jonathan.suever@rigor.com>
2017-12-22 11:57:06 -0500

    Version bump to 2.1.9

--- gui/__init__.py
+++ gui/__init__.py
@@ -21,7 +21,7 @@ if os.name == 'nt':
 else:
     configFile = os.path.join(os.getenv("HOME"), '.dicomSort.ini')
 
-Version = (2, 1, 8)
+Version = (2, 1, 9)
 __version__ = '.'.join([str(x) for x in Version])
 
 configVersion = '2.0'
