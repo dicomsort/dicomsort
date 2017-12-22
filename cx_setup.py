dea9331618dda4462d9003d0e29ae9b23a12b6cc
Jonathan Suever <jonathan.suever@rigor.com>
2017-12-22 11:56:55 -0500

    Update cx_setup.py to use latest cx_freeze version

--- cx_setup.py
+++ cx_setup.py
@@ -19,7 +19,7 @@ includefiles = [ICON,]
 NAME = 'DICOM Sort'
 VER = gui.__version__
 ID = '{{6638951C-0E99-4FAF-AAF1-B283912E7DE8}'
-URL = 'http://www.dicomsort.com'
+URL = 'https://dicomsort.com'
 
 if sys.platform == 'darwin':
     OUTDIR = os.path.join('dist',NAME)
@@ -28,13 +28,12 @@ else:
 
 build_options = {
     "build_exe": OUTDIR
-	}
-	
-build_exe_options = {
+}
+
+bdist_exe_options = {
     "include_msvcr": True,
-    "icon":ICON,
     "include_files": includefiles
-            }
+}
 
 exe = Executable(
     script='DicomSort.pyw',
@@ -42,79 +41,21 @@ exe = Executable(
     targetName=EXE,
     icon=ICON)
 
-bdist_mac_options = {'iconfile':ICON}
-bdist_dmg_options = {'volume_label':'DICOM Sort'}
-#"iconfile": 'DSicon.icns'}
+bdist_mac_options = {
+    'iconfile': ICON
+}
+
+bdist_dmg_options = {
+    'volume_label': NAME
+}
 
 setup(name = NAME,
       version = VER,
       description = 'A DICOM Sorting Utility',
-      options = {"build": build_options,
-	             "build_exe": build_exe_options,
-                 "bdist_mac": bdist_mac_options,
-                 "bdist_dmg": bdist_dmg_options},
+      options = {
+          "build": build_options,
+          "bdist_exe": bdist_exe_options,
+          "bdist_mac": bdist_mac_options,
+          "bdist_dmg": bdist_dmg_options
+      },
       executables=[exe])
-
-if sys.platform == 'win32':
-
-    INSTALLER = '%s_%s.setup' % (EXE.replace('.exe', ''), VER.replace('.', '_'))
-    INSTALLER = INSTALLER.replace(' ', '_')
-
-    # Construct the configuration string for Inno Setup
-    innoDict = {'AppId': ID,
-                'AppName': NAME,
-                'AppVerName': '%s %s' % (NAME, VER),
-                'AppPublisher': 'Jonathan Suever',
-                'AppPublisherURL': URL,
-                'AppSupportURL': URL,
-                'AppUpdatesURL': URL,
-                'DefaultDirName': '{pf}\%s' % NAME,
-                'DefaultGroupName': NAME,
-                'OutputDir': 'dist',
-                'OutputBaseFilename': INSTALLER,
-                'Compression': 'lzma',
-                'SolidCompression': 'yes'}
-
-    innoinput = '[Setup]\n'
-
-    for key, value in innoDict.items():
-        innoinput = ''.join([innoinput, '%s=%s\n' % (key, value)])
-
-    lang = '[Languages]\nName: "english"; MessagesFile: "compiler:Default.isl"\n'
-    task = '[Tasks]\nName:"desktopicon"; Description: "{cm:CreateDesktopIcon}";GroupDescription:"{cm:AdditionalIcons}";\n'
-
-    OUTEXE = os.path.join(OUTDIR, EXE)
-    INCLUDES = os.path.join(OUTDIR, '*')
-
-    files = '''[Files]
-    Source: "%s"; DestDir: "{app}"; Flags: ignoreversion
-    Source: "%s"; DestDir: "{app}"; Flags: ignoreversion recursesubdirs createallsubdirs'''
-
-    files = files % (OUTEXE, INCLUDES)
-
-    ICONFILE = ICON
-
-    icons = '''[Icons]
-    Name: "{group}\%s"; Filename: "{app}\%s"; IconFilename: {app}\%s; IconIndex: 0; WorkingDir: "{app}"
-    Name: "{userdesktop}\%s"; Filename: "{app}\%s"; Tasks: desktopicon; IconFilename: {app}\%s; IconIndex: 0'''
-
-    icons = icons % (NAME, EXE, ICONFILE, NAME, EXE, ICONFILE)
-
-    run = '''[Run]
-    Filename: "{app}\%s"; Description: "{cm:LaunchProgram,%s}"; Flags: nowait postinstall skipifsilent'''
-
-    run = run % (EXE, NAME)
-
-    fullfile = '\n'.join([innoinput, lang, task, files, icons, run])
-
-    # Write configuration to a temporary file
-    f = open('wizard.iss', 'w')
-    f.write(fullfile)
-    f.close()
-
-    print('Now Compiling using Inno Setup 5.')
-    print('Be sure that it is installed, and added to your system PATH')
-    os.system('Compil32 /cc wizard.iss')
-    os.remove('wizard.iss')
-
-print('Successfully created application and installer!')
