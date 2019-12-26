c0ab024f6147e230dfae8936014a6d652e4cec39
Jonathan Suever <suever@gmail.com>
2019-12-26 12:46:43 -0500

    Update gui.__init__

--- dicomsort/gui/__init__.py
+++ dicomsort/gui/__init__.py
@@ -1,7 +1,10 @@
-from __future__ import print_function
-
 from dicomsort.gui.core import DicomSort
 
-if __name__ == '__main__':
+
+def run():
     app = DicomSort(0)
     app.MainLoop()
+
+
+if __name__ == '__main__':
+    run()
