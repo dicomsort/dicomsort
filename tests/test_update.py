60251b328d8e84dee5cc80076c297886186e168f
Jonathan Suever <suever@gmail.com>
2020-02-07 19:53:03 -0500

    Add tests for remaining update methods

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,7 +1,28 @@
 import pytest
 
+import dicomsort
 from dicomsort.errors import InvalidVersion
-from dicomsort.update import version_tuple
+from dicomsort.update import version_tuple, latest_release, GITHUB_RELEASE_API, update_available
+
+
+class TestLatestRelease:
+    URL = GITHUB_RELEASE_API % {'repo': dicomsort.__repository__}
+
+    def test_happy_path(self, requests_mock):
+        version = 'v1.2.3'
+        requests_mock.get(self.URL, json={'tag_name': version})
+
+        assert latest_release() == version
+
+    def test_invalid_payload(self, requests_mock):
+        requests_mock.get(self.URL, json={})
+
+        assert latest_release() is None
+
+    def test_404(self, requests_mock):
+        requests_mock.get(self.URL, status_code=404)
+
+        assert latest_release() is None
 
 
 class TestVersionTuple:
@@ -20,4 +41,25 @@ class TestVersionTuple:
     def test_invalid_version(self):
         with pytest.raises(InvalidVersion):
             version_tuple('ref')
+
+
+class TestUpdateAvailable:
+    URL = GITHUB_RELEASE_API % {'repo': dicomsort.__repository__}
+
+    def test_new_version(self, requests_mock):
+        version = 'v100.0.0'
+        requests_mock.get(self.URL, json={'tag_name': version})
+
+        assert update_available() == (True, version)
+
+    def test_old_version(self, requests_mock):
+        version = 'v0.0.0.'
+        requests_mock.get(self.URL, json={'tag_name': version})
+
+        assert update_available() == (False, None)
+
+    def test_no_version(self, requests_mock):
+        requests_mock.get(self.URL, status_code=404)
+
+        assert update_available() == (False, None)
 
